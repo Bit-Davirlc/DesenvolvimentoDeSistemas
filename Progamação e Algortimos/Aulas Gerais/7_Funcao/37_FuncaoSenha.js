bae8c49e50f15e7e03855f13299df4d9e1763207
Aluno <e023aluno@outlook.com>
2026-04-29 21:50:50 -0300

    Função Login

---
+++ Progamação e Algortimos/Aulas Gerais/7_Funcao/37_FuncaoSenha.js
@@ -0,0 +1,21 @@
+const prompt = require("readline-sync");
+const alert = console.log;
+
+const verUsuario = "admin";
+const verSenha = "1234";
+
+function login(usuario, senha) {
+  return usuario === verUsuario && senha === verSenha;
+}
+
+while (true) {
+  let usuario = prompt.question("Digite o usuario: ");
+  let senha = prompt.question("Digite a senha: ");
+
+  if (login(usuario, senha)) {
+    alert("Login confirmado!");
+    break;
+  } else {
+    alert("Login negado!");
+  }
+}
