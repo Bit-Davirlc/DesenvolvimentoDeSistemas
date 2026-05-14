8cd70336564f3bc7d4dbd5364428a8279aec4b08
Aluno <e023aluno@outlook.com>
2026-05-13 22:08:16 -0300

    HamburgueriaFinal

--- Progamação e Algortimos/Aulas Gerais/8_Array/43_MenuArray.js
+++ Progamação e Algortimos/Aulas Gerais/8_Array/43_MenuArray.js
@@ -1,7 +1,6 @@
 const prompt = require("readline-sync");
 const alert = console.log;
 
-let select = 0;
 let hamburgueria = [];
 
 function pausar() {
@@ -20,7 +19,58 @@ function exibirMenu() {
 function adicionarHamburguer() {
   let continuar = "s";
 
+  console.clear();
+  alert(`1. Adicionar Hamburguer\n`);
+
   while (continuar.toLowerCase() === "s") {
+    const nome = prompt.question(`Nome do Hamburguer: `).trim();
+    const preco = Number(prompt.question(`Preco do Hamburguer: `));
+
+    if (nome === "" || isNaN(preco) || preco <= 0) {
+      alert(`Dados inválidos!`);
+    } else {
+      const hamburguer = {
+        nome: nome,
+        preco: preco,
+      };
+
+      hamburgueria.push(hamburguer);
+
+      alert(`\nHamburguer adicionado com sucesso.`);
+    }
+
+    continuar = prompt.question(`Deseja adicionar outro Hamburguer (s/n)?: `);
+  }
+}
+
+function verHamburguer() {
+  console.clear();
+  alert(`2. Ver todos os Hamburgueres\n`);
+
+  if (hamburgueria.length === 0) {
+    alert(`Nenhum hamburguer registrado.`);
+  } else {
+    for (let i = 0; i < hamburgueria.length; i++) {
+      alert(
+        `ID: ${i + 1}. Nome: ${hamburgueria[i].nome} | Preco: ${hamburgueria[i].preco}`,
+      );
+    }
+  }
+}
+
+function atualizarHamburguer() {
+  console.clear();
+  alert(`3. Atualizar Hamburguer\n`);
+
+  verHamburguer();
+
+  let attSelect = Number(
+    prompt.question(`\nDigite o ID do hamburguer que pretende atualizar: `),
+  );
+
+  let indiceReal = attSelect - 1;
+
+  if (indiceReal >= 0 && indiceReal < hamburgueria.length) {
     const nome = prompt.question(`Nome do Hamburguer: `);
     const preco = Number(prompt.question(`Preco do Hamburguer: `));
     const hamburguer = {
@@ -28,41 +78,69 @@ function adicionarHamburguer() {
       preco: preco,
     };
 
-    hamburgueria.push(hamburguer);
+    hamburgueria[indiceReal] = hamburguer;
 
-    alert(`\nHamburguer adicionado com sucesso.`);
-
-    continuar = prompt.question(`Deseja adicionar outro Hamburguer (s/n)?: `);
+    alert(`\nHamburguer ${attSelect} atualizado com sucesso!`);
+  } else {
+    alert(`\nID invalido!`);
   }
 }
 
-function verHamburguer() {
-  if (hamburgueria.length === 0) {
-    alert(`\nNenhum hamburguer registrado.`);
+function deletarHamburguer() {
+  console.clear();
+
+  verHamburguer();
+
+  alert(`\n4. Deletar Hamburguer\n`);
+
+  let attSelect = Number(
+    prompt.question(`\nDigite o ID do hamburguer que pretende deletar: `),
+  );
+
+  let indiceReal = attSelect - 1;
+
+  if (indiceReal >= 0 && indiceReal < hamburgueria.length) {
+    hamburgueria.splice(indiceReal, 1);
+
+    alert(`\nHamburguer ${attSelect} deletado com sucesso!`);
   } else {
-    for (let i = 0; i < hamburgueria.length; i++) {
-      alert(
-        `${i + 1}. Nome: ${hamburgueria[i].nome} | Preco: ${hamburgueria[i].preco}`,
-      );
+    alert(`\nID invalido!`);
+  }
+}
+
+function principal() {
+  let select = 0;
+
+  while (select != 5) {
+    console.clear();
+    exibirMenu();
+    select = prompt.question("\nQual opcao deseja selecionar: ");
+
+    switch (select) {
+      case "1":
+        adicionarHamburguer();
+        pausar();
+        break;
+      case "2":
+        verHamburguer();
+        pausar();
+        break;
+      case "3":
+        atualizarHamburguer();
+        pausar();
+        break;
+      case "4":
+        deletarHamburguer();
+        pausar();
+        break;
+      case "5":
+        break;
+      default:
+        alert(`Opcao invalida!`);
+        pausar();
+        break;
     }
   }
 }
 
-while (select != 5) {
-  console.clear();
-  exibirMenu();
-  select = prompt.question("\nQual opcao deseja selecionar: ");
-
-  switch (select) {
-    case "1":
-      adicionarHamburguer();
-      pausar();
-      break;
-    case "2":
-      verHamburguer();
-      pausar();
-      break;
-    case "5":
-      break;
-  }
-}
+principal();
